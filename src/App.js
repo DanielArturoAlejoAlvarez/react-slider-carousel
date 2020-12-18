bf1a842aff94f461609099e5c32f6ae77b51b750
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-18 09:44:52 -0500

    Creating our components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import ImageSlider from './components/ImageSlider';
 
 function App() {
   return (
-    <div>
-      <h1>Slider</h1>
-    </div>
+    <>
+      <ImageSlider />
+    </>
   );
 }
 
---
+++ src/components/ImageSlider.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ImageSlider = () => {
+  return (
+    <div>
+      <h1>Slider</h1>
+    </div>
+  )
+}
+
+export default ImageSlider
