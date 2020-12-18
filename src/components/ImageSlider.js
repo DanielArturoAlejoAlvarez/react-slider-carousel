72156f4aca9be1f5be2d5ba52aaad9a65ec8e90d
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2020-12-18 11:52:06 -0500

    Creating apllication logic

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { DataSlider } from './DataSlider'
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
+//import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
 const ImageSlider = ({slides}) => {
 
@@ -9,19 +10,28 @@ const ImageSlider = ({slides}) => {
   const length = slides.length
 
   const prevSlide = ()=>{
-    
+    setCurrent(current === 0 ? length - 1 : current - 1)
   }
-  const nextSlide = ()=>{
 
+  const nextSlide = ()=>{
+    setCurrent(current === length - 1 ? 0 : current + 1)
+  }
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
   }
 
 
   return (
     <section className="slider">
-      <FaArrowAltCircleLeft className="arrow-left" onClick={prevSlide} />
-      <FaArrowAltCircleRight className="arrow-right" onClick={nextSlide} />
+      <IoIosArrowBack className="arrow-left" onClick={prevSlide} />
+      <IoIosArrowForward className="arrow-right" onClick={nextSlide} />
       {DataSlider.map((slide,index)=>(
-        <img src={slide.image} alt="Ferrari image" />
+        <div className={index === current ? 'slide active' : 'slide'} key={index}>
+          {index === current && (
+            <img src={slide.image} alt="Ferrari image" className="image" />
+          )}
+        </div>
       ))}
     </section>
   )
